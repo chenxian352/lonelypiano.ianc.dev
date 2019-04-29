25ff2bda5afeceddd6ffb17ed6952c16112819f8
ianchen <chenxian352@gmail.com>
2019-04-29 14:23:21 +1200

    complete: submit form. todo: google map and vue.

---
+++ script.js
@@ -0,0 +1,8 @@
+var submitPianoButton = document.querySelector('.submit-piano');
+const popup = document.querySelector('.popup');
+
+submitPianoButton.onclick = function() {
+  popup.classList.toggle('hidden');
+};
+
+
